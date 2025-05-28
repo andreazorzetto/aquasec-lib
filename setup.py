ba515cca4590171eb45d1c16b5cbebdac8818869
Andrea Zorzetto <andrea@thatpersonalmail.com>
2025-05-28 10:45:13 +0100

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='aquasec',
-    version='0.1.0',
+    version='0.1.1',
     author='Andrea Zorzetto',
     author_email='your-email@example.com',
     description='API client library for Aqua Security platform',
